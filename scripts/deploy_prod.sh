cbfbc7fa9012d6a49904d7230f1f5f18b434bed5
Xinyu Wang <30540047@qq.com>
2021-10-25 16:02:26 +0800

    s

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -10,7 +10,7 @@ do
 array=(miscellaneous)
 for server in ${array[@]}
 do
-python /opt/works/cd_js_to_cos/download_package.py $SECRET_ID $SECRET_KEY ${server}-1.0.0-$GO_PIPELINE_LABEL.jar
+python /opt/works/cd_js_to_cos/download_package.py $SECRET_ID $SECRET_KEY ${server}-1.0-$GO_PIPELINE_LABEL.jar
 
 pid=$(ssh root@$remote_host "ps -ef | grep '${server}'| grep -v grep | awk  '{print $2}'"| awk '{print $2}')
 if [ ! $pid ]; then
@@ -20,8 +20,8 @@ else
 	ssh root@$remote_host "kill -9 ${pid}"
 fi
 
-scp ${server}-1.0.0-$GO_PIPELINE_LABEL.jar root@$remote_host:/tmp/
-ssh root@$remote_host "mv -f /tmp/${server}-1.0.0-${GO_PIPELINE_LABEL}.jar /opt/works/deployment"
+scp ${server}-1.0-$GO_PIPELINE_LABEL.jar root@$remote_host:/tmp/
+ssh root@$remote_host "mv -f /tmp/${server}-1.0-${GO_PIPELINE_LABEL}.jar /opt/works/deployment"
 done
 
 ssh root@$remote_host /bin/bash << EOF
@@ -30,8 +30,8 @@ source ~/.jdk8.rc
 cd /opt/works/deployment
 
 
-echo 'nohup java -jar miscellaneous-1.0.0-$GO_PIPELINE_LABEL.jar  --spring.profiles.active=prod --server.port=8091 > /dev/null 2>&1 &'
-nohup java -jar miscellaneous-1.0.0-$GO_PIPELINE_LABEL.jar  --spring.profiles.active=prod --server.port=8091 > hole-config.log 2>&1 &
+echo 'nohup java -jar miscellaneous-1.0-$GO_PIPELINE_LABEL.jar  --spring.profiles.active=prod --server.port=8091 > /dev/null 2>&1 &'
+nohup java -jar miscellaneous-1.0-$GO_PIPELINE_LABEL.jar  --spring.profiles.active=prod --server.port=8091 > hole-config.log 2>&1 &
 
 
 EOF

5553003117d3e161d67a469e852f518517feee31
Xinyu Wang <30540047@qq.com>
2023-07-07 10:43:20 +0800

    test

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -3,7 +3,7 @@ set -e
 
 whoami
 source ~/.bash_profile
-source ~/.jdk8.rc
+#source ~/.jdk8.rc
 mvn clean package
 
-python /opt/works/cd_js_to_cos/upload_package_to_cos.py $SECRET_ID $SECRET_KEY target/miscellaneous-1.0-SNAPSHOT.jar miscellaneous-1.0-SNAPSHOT-$GO_PIPELINE_LABEL.jar
+#python /opt/works/cd_js_to_cos/upload_package_to_cos.py $SECRET_ID $SECRET_KEY target/miscellaneous-1.0-SNAPSHOT.jar miscellaneous-1.0-SNAPSHOT-$GO_PIPELINE_LABEL.jar
